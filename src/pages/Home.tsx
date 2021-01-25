a253eaa77c56d5affae259ac52b9a4d78d07a5ac
Bilal Raza <7682122+bilalrazamoon@users.noreply.github.com>
2021-01-25 06:14:21 +0500

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import './Home.css';
+
+import React from 'react';
+
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+import ExploreContainer from '../components/ExploreContainer';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Ionic React App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Ionic React App</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
